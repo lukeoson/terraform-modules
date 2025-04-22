faeed77bfb64cfd5e37416393aec4620ff6ab6f9
Luke Richardson <lukeoson@gmail.com>
2025-04-22 12:33:10 +0100

    s3 backend format spacing only

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -15,12 +15,9 @@ terraform {
   }
 }
 
-
-
 #remote state
 data "terraform_remote_state" "db" {
   backend = "s3"
-
   config = {
     bucket = var.db_remote_state_bucket
     key    = var.db_remote_state_key
